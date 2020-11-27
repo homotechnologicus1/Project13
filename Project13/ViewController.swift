3a225e8f595e1000f9b72a9f9ec8b1823d9f9ef1
joe <homo.technologicus1@gmail.com>
2020-11-27 12:18:38 +0900

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by joe_mac on 11/27/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
